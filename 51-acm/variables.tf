db4c9aeb7231bdd80f8d3ced219f9d2fdb025681
sowmyataraka <sowmya.gundaji@gmail.com>
2026-08-05 16:21:37 +0100

    k8-ingress

---
+++ 51-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "daws90s.uk"
+}
+
+variable "zone_id" {
+    default = "Z01760881W37AC9NAJ055"
+}  
